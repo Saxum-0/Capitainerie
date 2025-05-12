5f087d6c693365f71ba5d5bafebb5ef5aa644776
Saxum-0 <165333677+Saxum-0@users.noreply.github.com>
2025-05-12 17:57:33 +0200

    kn

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,21 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  base: '/', // ✅ base reste utile
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects',
-          dest: '.' // copié à la racine de dist/
-        }
-      ]
-    })
   ],
   resolve: {
     alias: {
@@ -24,3 +15,4 @@ export default defineConfig({
     },
   },
 })
+
